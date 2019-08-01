8d4b1b5e306b3e89dcf414b9c493c2842a5a9bba
jonathanfalcon95 <jonathan.falcon@tmconsulting.mx>
2019-08-01 18:42:55 -0400

    first commit

---
+++ ApiTest/Controllers/UserController.cs
@@ -0,0 +1,78 @@
+﻿using ApiTest.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/users")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly UserContext _context;
+
+        public UserController(UserContext context)
+        {
+            _context = context;
+
+            if (_context.Users.Count() == 0)
+            {
+                // Create a new TodoItem if collection is empty,
+                // which means you can't delete all TodoItems.
+                _context.Users.Add(new User { Name = "Juan", UserName = "JPerez", LastName = "Perez", Age = 25 });
+                _context.Users.Add(new User { Name = "Maria", UserName = "MHernandez", LastName = "Hernadez", Age = 21 });
+
+                _context.SaveChanges();
+            }
+        }
+
+        // GET: api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<User>>> GetUser()
+        {
+            return await _context.Users.ToListAsync();
+        }
+
+        // GET: api/users/1
+        [HttpGet("{id}")]
+        public async Task<ActionResult<User>> GetUser(long id)
+        {
+            var user = await _context.Users.FindAsync(id);
+
+            if (user == null)
+            {
+                return NotFound();
+            }
+
+            return user;
+        }
+
+
+        // POST: api/users
+        [HttpPost]
+        public async Task<ActionResult<User>> PostUser(User user)
+        {
+            _context.Users.Add(user);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction(nameof(GetUser), new { id = user.Id }, user);
+        }
+
+        // PUT: api/user/2
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutUser(long id, User user)
+        {
+            if (id != user.Id)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(user).State = EntityState.Modified;
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+    }
+}
---
+++ ApiTest/Models/User.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ApiTest.Models
+{
+    public class User
+    {
+        public long Id { get; set; }
+        public string UserName { get; set; }
+        public string Name { get; set; }
+        public string LastName { get; set; }
+        public int Age { get; set; }
+        public bool IsComplete { get; set; }
+        public DateTime LastSessionDateTime { get; set; }
+
+    }
+}
